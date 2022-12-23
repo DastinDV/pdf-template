9c731133d2bdbf7c66432df38d6611dfb5127e80
DastinDV <komander_72@rambler.ru>
2022-12-23 19:30:44 +0300

    Added pdf generator by topics from csv

---
+++ main.py
@@ -0,0 +1,17 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    print(row["Topic"])
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100,100,100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 30, 200, 30)
+
+pdf.output("output.pdf")
